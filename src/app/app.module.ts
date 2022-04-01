71d52318c0a2a6d33f1eb489c2b4ab872995b8f8
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-04-01 17:29:32 -0300

    "Cree componente button"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
