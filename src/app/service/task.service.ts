da8e34edf7f53be8429089e46daab6f9c2256580
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-04-02 20:41:51 -0300

    Vinculado a json

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { TASK } from '../mock-task';
 import { Task } from '../Task';
@@ -8,10 +9,13 @@ import { Task } from '../Task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(
+    private http:HttpClient
+  ) { }
 
   getTask(): Observable<Task[]>{
-    const task = of(TASK);
-    return task;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
