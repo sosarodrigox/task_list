55fce1f619827c1166336f81d1f43034550a99eb
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-04-02 16:09:02 -0300

    Cree el mock de tareas mosk-task y la interface

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASK: Task[] =[
+    {
+        id:1,
+        text: "Finish first angular module",
+        day:'August 5 - 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Buy a headphones",
+        day:'April 4 - 13:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Investigate boostrap",
+        day:'July 19 - 18:00',
+        reminder: false
+    },
+    {
+        id:4,
+        text: "Read a book",
+        day:'June 28 - 24:00',
+        reminder: true
+    }
+]
